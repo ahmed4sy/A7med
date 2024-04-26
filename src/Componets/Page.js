727cfcb212bd1545bda48dc492f404b32e7d7980
ahmed4sy <hjlook99@gmail.com>
2024-04-26 11:37:06 +0300

    list

--- src/Componets/Page.js
+++ src/Componets/Page.js
@@ -41,14 +41,16 @@ export default function Page({ children }) {
         <h1 className="Title-Page">A7med</h1>
 
         {/* List */}
-        <h1
+        <div
           className="List-Page"
           onClick={() => {
             setOnlis(Onlis ? false : true);
           }}
         >
-          {!Onlis ? "≈" : "X"}
-        </h1>
+          <span className={Onlis ? "keyframespan1 control" : "none"}></span>
+          <span className={Onlis ? "hide" : "none"}></span>
+          <span className={Onlis ? "keyframespan2 control" : "none"}></span>
+        </div>
         <ListPage list={Onlis} />
       </div>
     </div>
