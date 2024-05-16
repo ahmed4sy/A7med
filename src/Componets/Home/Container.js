43840862f0a874e201a79da99f86c3275ab63137
ahmed4sy <hjlook99@gmail.com>
2024-05-16 09:56:13 +0300

    data

--- src/Componets/Home/Container.js
+++ src/Componets/Home/Container.js
@@ -1,16 +1,65 @@
+import { useState } from "react";
 export default function Template({ children }) {
   // temp
+
   return (
     <div className="Con color-back">
       <h3>Templates:</h3>
       {children}
       <h3
         style={{
-          marginTop: 350,
+          marginTop: 320,
         }}
       >
         New Update:
       </h3>
+      <Almost />
     </div>
+  );
+}
+function Almost(params) {
+  let [onMoveDownUpdate, setMoveDownUpdate] = useState(false);
+  return (
+    <>
+      <div className="AlmostDesing">
+        {onMoveDownUpdate ? (
+          <p
+            style={{
+              position: "fixed",
+              bottom: 60,
+              paddingLeft: 60,
+              fontSize: 45,
+            }}
+          >
+            Almost finished
+          </p>
+        ) : (
+          ""
+        )}
+        <div
+          style={
+            onMoveDownUpdate
+              ? {
+                  height: "85%",
+                }
+              : {}
+          }
+        ></div>
+      </div>
+
+      <div
+        onMouseMoveCapture={() => setMoveDownUpdate(true)}
+        onMouseOutCapture={() => setMoveDownUpdate(false)}
+        style={{
+          position: "fixed",
+          width: 450,
+          height: 140,
+          bottom: 50,
+          marginLeft: 25,
+          borderRadius: 25,
+          cursor: "pointer",
+        }}
+      ></div>
+    </>
   );
 }
